46d7078beb8fd9dad1863d8d71f49df1d3dfba80
Peter Elmers <peter.elmers@yahoo.com>
2021-07-27 00:54:37 +0000

    move ratio to a constants file

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,6 @@ const ID = 't';
 export const SPACE_MARKER = `/* ${ID}*/`;
 export const OPTIONAL_SPACE_MARKER = `/*opt-${ID}*/`;
 export const UNBREAKABLE_SPACE_MARKER = `/* ubn-${ID}*/`;
+
+// This constant is a guess at a typical font's ratio of height:width
+export const DEFAULT_HEIGHT_WIDTH_RATIO = 1.7;
--- src/shapes/square.ts
+++ src/shapes/square.ts
@@ -1,10 +1,10 @@
+import { DEFAULT_HEIGHT_WIDTH_RATIO } from '../constants';
 import { minCodeSize, parseTokens, reshape } from '../reshape';
 
-const heightWidthRatio = 1.7;
-
+// TODO: probably makes sense to have ratio as an optional input param
 export function square(code: string): string {
     const tokens = parseTokens(code);
     const codeSize = minCodeSize(tokens);
-    const sideLength = heightWidthRatio * Math.sqrt(codeSize);
+    const sideLength = DEFAULT_HEIGHT_WIDTH_RATIO * Math.sqrt(codeSize);
     return reshape(tokens, () =>sideLength);
 }
