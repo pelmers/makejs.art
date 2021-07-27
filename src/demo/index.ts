00e70ccd66f8b8322b2058c73110286d2c7342fc
Peter Elmers <peter.elmers@yahoo.com>
2021-07-27 03:20:16 +0000

    remove demo page stuff (to gh-pages branch)

--- src/demo/index.ts
+++
@@ -1,5 +0,0 @@
-const $text = document.querySelector('#code-text');
-
-// TODO: on button click, reshape contents of $text
-// TODO: probably want to run input through babel to cleanup any too-fancy syntax
-// TODO: move this to gh-pages branch?
