668a0b7c52c31d1499763068d2112683fc5edd94
Peter Elmers <peter.elmers@yahoo.com>
2021-08-03 18:38:05 +0000

    add lru cache

--- src/demo/algos/saliency.ts
+++ src/demo/algos/saliency.ts
@@ -2,6 +2,7 @@ import { extractRunsByCutoff, SALIENCY_BUCKETS } from './common';
 
 // @ts-ignore no types provided
 import { Colour } from '../vendor/IsThisColourSimilar/Colour';
+import LRUCache from 'lru-cache';
 
 // Retain the top-n histogram buckets to reach THRESHOLD coverage of the picture.
 // Fold anything that doesn't reach the threshold to the nearest bucket, by manhattan distance.
@@ -29,6 +30,20 @@ const indexToColor: [number, number, number][] = new Array(
         ];
     });
 
+const rgbCache = new LRUCache({ max: 2 * Math.pow(SALIENCY_BUCKETS, 3) });
+
+function rgb2lab(r: number, g: number, b: number) {
+    const key = [r, g, b].join(' ');
+    const res = rgbCache.get(key);
+    if (res != null) {
+        return res;
+    } else {
+        const lab = Colour.rgba2lab(r, g, b);
+        rgbCache.set(key, lab);
+        return lab;
+    }
+}
+
 const diff = (
     r1: number,
     g1: number,
@@ -37,13 +52,8 @@ const diff = (
     g2: number,
     blu2: number
 ) => {
-    // TODO
-    // literally 90% of the time in these functions... LOL
-    // well i can cache the histogram's lab values since those don't change
-    // that would save about 25% time
-    // and we can compress the histogram to save about 10x more time...
-    const [l1, a1, b1] = Colour.rgba2lab(r1, g1, blu1);
-    const [l2, a2, b2] = Colour.rgba2lab(r2, g2, blu2);
+    const [l1, a1, b1] = rgb2lab(r1, g1, blu1);
+    const [l2, a2, b2] = rgb2lab(r2, g2, blu2);
     return Colour.deltaE00(l1, a1, b1, l2, a2, b2);
 };
 
@@ -69,14 +79,14 @@ class CompressedHistogram {
             const { v, idx } = sorted[i];
             // add the remaining values to their closest neighbors in the histogram
             const [r1, g1, blu1] = indexToColor[idx];
-            const [l1, a1, b1] = Colour.rgba2lab(r1, g1, blu1);
+            const [l1, a1, b1] = rgb2lab(r1, g1, blu1);
             let smallestDelta = Number.MAX_SAFE_INTEGER;
             let smallestDeltaIdx = 0;
             // find the closest color by perceptual difference
             for (const { delta, idx } of this.compressed.map(
                 ({ originalIndex }, idx) => {
                     const [r2, g2, blu2] = indexToColor[originalIndex];
-                    const [l2, a2, b2] = Colour.rgba2lab(r2, g2, blu2);
+                    const [l2, a2, b2] = rgb2lab(r2, g2, blu2);
                     return { delta: Colour.deltaE00(l1, a1, b1, l2, a2, b2), idx };
                 }
             )) {
@@ -99,7 +109,6 @@ class CompressedHistogram {
     saliency(r: number, g: number, b: number) {
         const pixelIndex = colorToIndex(r, g, b);
         const compressedPixelIndex = this.originalToCompressedMap.get(pixelIndex)!;
-        // TODO also add the smoothing factor
         return this.compressed.reduce((prev, cur, idx) => {
             if (idx === compressedPixelIndex) {
                 return prev;
@@ -129,16 +138,14 @@ export function findRegionsBySaliency(
         const [r, g, b] = data.data.slice(i, i + 3);
         colorHistogram[colorToIndex(r, g, b)]++;
     }
-    // TODO threshold the color histogram by keeping 95% of the pixels
     const histogram = new CompressedHistogram(colorHistogram);
     const perPixelSaliency: number[] = new Array(canvas.width * canvas.height);
     let maxSaliency = 1;
     for (let i = 0; i < data.data.length; i += 4) {
         const [r, g, b] = data.data.slice(i, i + 3);
-        perPixelSaliency[i / 4] = histogram.saliency(r,g,b);
+        perPixelSaliency[i / 4] = histogram.saliency(r, g, b);
         maxSaliency = Math.max(maxSaliency, perPixelSaliency[i / 4]);
     }
-    // TODO implement quantization smoothingg
     const sortedPixels = perPixelSaliency
         .concat()
         .sort((a, b) => (invert ? a - b : b - a));
@@ -175,43 +182,4 @@ export function findRegionsBySaliency(
             ? perPixelSaliency[i] <= cutoffValue
             : perPixelSaliency[i] >= cutoffValue;
     });
-}
-
-function deltaE(rgbA: number[], rgbB: number[]) {
-    let labA = rgb2lab(rgbA);
-    let labB = rgb2lab(rgbB);
-    let deltaL = labA[0] - labB[0];
-    let deltaA = labA[1] - labB[1];
-    let deltaB = labA[2] - labB[2];
-    let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
-    let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
-    let deltaC = c1 - c2;
-    let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
-    deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
-    let sc = 1.0 + 0.045 * c1;
-    let sh = 1.0 + 0.015 * c1;
-    let deltaLKlsl = deltaL / 1.0;
-    let deltaCkcsc = deltaC / sc;
-    let deltaHkhsh = deltaH / sh;
-    let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
-    return i < 0 ? 0 : Math.sqrt(i);
-}
-
-function rgb2lab(rgb: number[]) {
-    let r = rgb[0] / 255,
-        g = rgb[1] / 255,
-        b = rgb[2] / 255,
-        x,
-        y,
-        z;
-    r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
-    g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
-    b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
-    x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
-    y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.0;
-    z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
-    x = x > 0.008856 ? Math.pow(x, 1 / 3) : 7.787 * x + 16 / 116;
-    y = y > 0.008856 ? Math.pow(y, 1 / 3) : 7.787 * y + 16 / 116;
-    z = z > 0.008856 ? Math.pow(z, 1 / 3) : 7.787 * z + 16 / 116;
-    return [116 * y - 16, 500 * (x - y), 200 * (y - z)];
 }
